32caa533936f809e2f328922c67b0230dfdb3c59
raphael sena <rsenares1@gmail.com>
2025-02-03 18:01:21 -0300

    feat: add compound interest function

--- code/backend/src/main/java/com/raphaelsena/juros/services/ContaService.java
+++ code/backend/src/main/java/com/raphaelsena/juros/services/ContaService.java
@@ -3,13 +3,14 @@ package com.raphaelsena.juros.services;
 import com.raphaelsena.juros.models.Conta;
 import com.raphaelsena.juros.models.Item;
 import com.raphaelsena.juros.models.dtos.ContaCreateDTO;
-import com.raphaelsena.juros.models.enums.Tipo;
 import com.raphaelsena.juros.repositories.ContaRepository;
 import com.raphaelsena.juros.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,7 +36,8 @@ public class ContaService {
     @Transactional
     public Conta create(ContaCreateDTO obj) {
         Conta conta = new Conta();
-        conta.setValorTotal(0.0);
+        conta.setValorTotalSemJuros(0.0);
+        conta.setValorTotalComJuros(0.0);
         conta.setValorPendente(0.0);
         conta.setValorPago(0.0);
         conta = contaRepository.save(conta);
@@ -44,32 +46,60 @@ public class ContaService {
         List<Item> itens = obj.getItens().stream()
                 .map(item -> {
                     item.setConta(contaTemp);
+                    item.setJuros(calcularJuros(item));
+                    contaTemp.setValorTotalComJuros(contaTemp.getValorTotalComJuros() + item.getJuros());
+                    item.setPago(false);
                     return itemRepository.save(item);
                 }).collect(Collectors.toList());
 
+
         conta.setItens(itens);
-        conta.setValorTotal(calcularValorTotal(conta.getItens()));
+        conta.setValorTotalSemJuros(calcularValorTotalSemJuros(conta.getItens()));
 
         conta.setValorPago(calcularValorPago(conta));
+        conta.setValorTotalComJuros(contaTemp.getValorTotalComJuros());
         conta.setValorPendente(calcularValorPendente(conta));
+
         return contaRepository.save(conta);
     }
 
-    private Double calcularValorTotal(List<Item> itens) {
+    private Double calcularValorTotalSemJuros(List<Item> itens) {
+
         return itens.stream()
-                .filter(item -> (item.getTipo() == Tipo.ACRESCIMO))
                 .mapToDouble(Item::getValor)
                 .sum();
     }
 
     private Double calcularValorPago(Conta conta) {
         return conta.getItens().stream()
-                .filter(item -> item.getTipo() == Tipo.DEDUCAO)
                 .mapToDouble(Item::getValor)
                 .sum();
     }
 
     private Double calcularValorPendente(Conta conta) {
-        return conta.getValorTotal() - (conta.getValorPago() != null ? conta.getValorPago() : 0.0);
+        return conta.getValorTotalComJuros() - (conta.getValorPago() != null ? conta.getValorPago() : 0.0);
     }
+
+    private static final double TAXA_JUROS_DIARIA = 0.02;
+
+    private Double calcularJuros(Item item) {
+        double totalJuros = 0.0;
+
+            if (item.getDataLimitePagamento() != null && item.getDataPagamento() != null) {
+                if (item.getDataPagamento().isAfter(item.getDataLimitePagamento())) {
+                    long diasAtraso = ChronoUnit.DAYS.between(item.getDataLimitePagamento(), LocalDate.now());
+
+                    double saldoDevedor = item.getValor();
+
+                    // formula juros compostos: M = P * (1 + i)^t
+                    double jurosItem = saldoDevedor * (Math.pow(1 + TAXA_JUROS_DIARIA, diasAtraso) - 1);
+
+                    totalJuros += jurosItem;
+                }
+            }
+
+
+        return totalJuros;
+    }
+
 }
