fe667458603dd52c42e9283604b9f2de2ae10c39
raphael sena <rsenares1@gmail.com>
2025-02-03 11:23:24 -0300

    feat: create java spring project

---
+++ code/backend/src/main/java/com/raphaelsena/juros/JurosApplication.java
@@ -0,0 +1,13 @@
+package com.raphaelsena.juros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurosApplication.class, args);
+	}
+
+}
---
+++ code/backend/src/test/java/com/raphaelsena/juros/JurosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphaelsena.juros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
