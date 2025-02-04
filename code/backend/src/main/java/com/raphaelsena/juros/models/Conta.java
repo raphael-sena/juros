ebff3bf94de550d25412b5b3cbcb15cbcfd56057
raphael sena <rsenares1@gmail.com>
2025-02-04 15:45:45 -0300

    style: imports specific classes

--- code/backend/src/main/java/com/raphaelsena/juros/models/Conta.java
+++ code/backend/src/main/java/com/raphaelsena/juros/models/Conta.java
@@ -1,8 +1,18 @@
 package com.raphaelsena.juros.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
